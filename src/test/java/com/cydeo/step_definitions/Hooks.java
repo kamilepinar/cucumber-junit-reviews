c79d2faa7e1559913b7ec253166f304809639682
AdamB25 <barsialtunit@gmail.com>
2023-02-18 14:45:30 -0500

    Added Screenshot

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -4,6 +4,9 @@ import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -15,7 +18,13 @@ public class Hooks {
 
 
     @After  //it should come form cucumber java NOT JUNIT
-    public void tearDown()  {
+    public void tearDown(Scenario scenario)  {
+
+      //  if (scenario.isFailed()){
+            byte[] data = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data,"image/png", scenario.getName());
+        //}
+
 
         BrowserUtils.sleep(3);
         Driver.closeDriver();
